9fb03c6de8bd7c2a02e99c1d50725825d4eb16e2
Andres <orlando.raul.andres@outlook.com>
2024-09-18 09:49:27 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "Installing Nodejs"
 
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs libfuse2
 
 echo "Installing Neovim, cloning repo and installing plugins"
 
@@ -11,7 +11,7 @@ if [ ! -d ~/.local/bin ]; then
 	mkdir ~/.local/bin
 fi
 
-wget -P ~/.local/bin/ https://github.com/neovim/neovim/releases/download/v0.9.0/nvim.appimage 
+wget -P ~/.local/bin/ https://github.com/neovim/neovim/releases/download/v0.10.1/nvim.appimage 
 
 export PATH=~/.local/bin:$PATH
 
